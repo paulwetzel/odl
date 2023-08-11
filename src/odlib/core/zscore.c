9a71691f46dc59c35feaaebc5e2bb6a1f07d284b
Paul Wetzel <post@paulwet.com>
2023-08-11 12:31:57 +0200

    Added files

--- src/odlib/core/zscore.c
+++ src/odlib/core/zscore.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+
+/*
+ * Calculates the mean of the dataset.
+ *
+ * Parameters:
+ *   data - List of datapoints that is passed to the function.
+ *   dimension - Dimension of the total dataset, e.g. dim1 = x, dim2 = x,y, dim3=x,y,z
+ *   length - Length of the dataset, e.g. how many elements are there?
+ *
+ * Returns:
+ *   mean (int) - the mean value of the whole dataset
+ *
+ * Example Usage:
+ *   int result = add(5, 7);
+ *   // result is now 12
+ */
+float mean_of_dataset(float* data, int length, int dimension) {
+    
+}
--- src/odlib/main.py
+++ src/odlib/main.py
@@ -17,3 +17,26 @@ def init_base_functions():
     """
     clib_main = ctypes.CDLL("src/odlib/core/main.so")
     clib_utils = ctypes.CDLL("src/odlib/core/main.so")
+
+    return clib_main, clib_utils
+
+def prepare_list(list_object, clib_to_use):
+    """
+    Prepare a python type list for sending it to a c function
+
+    When calling functions that are originally written in C, one can't simply pass a python list,
+    as datastructures in c remain much more strict. THis function takes in a list, alternativly np.array() will
+    work as well, and converts it into a format that will make it usable for a c function acessed by ctypes.
+
+    Args:
+        list_object (list or np.array): List of elements that should be converted 
+        clib_to_use (CDLL type object): CDLL that the list will be passed to
+
+    Returns:
+        c_list (ctypes.c): List ready to pass to a c function   
+    """
+
+    c_list = (ctypes.c_float * len(list_object))(*list_object)
+    clib_to_use.process
+
+
